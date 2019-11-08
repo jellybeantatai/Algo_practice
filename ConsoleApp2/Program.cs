4cc3063a2547a72562d7879c10c096c54ebc0ef3
jellybeantatai <abhinabakar@gmail.com>
2019-11-08 20:26:50 +0530

    First Commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,28 +8,9 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int[,] arr = { { 3, 4 }, { 2, 3 }, { 7, 8 } };
 
-            for(int i=0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    Console.Write("|"+arr[i,j]+"|");
-                }
-                Console.WriteLine("");
-                for (int k=0; k < arr.GetLength(1); k++)
-                {
-                    Console.Write("---");
-                }
-                Console.WriteLine("");
-            }
-
-            if (1 > null)
-            {
-                Console.WriteLine("haha");
-            }
         }
     }
 }
